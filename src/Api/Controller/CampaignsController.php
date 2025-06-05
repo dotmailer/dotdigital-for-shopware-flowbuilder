0e4b1d804bbef138c5bdc0e049fedd801d750594
Gustaf.Landman <gustaf.landman@dotdigital.com>
2025-06-05 13:31:38 +0200

    PHP attributes routing

--- src/Api/Controller/CampaignsController.php
+++ src/Api/Controller/CampaignsController.php
@@ -9,11 +9,9 @@ use Dotdigital\Flow\Service\Client\DotdigitalClientFactory;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-/**
- * @Route(defaults={"_routeScope"={"api"}})
- */
+#[Route(defaults: ['_routeScope' => ['api']])]
 class CampaignsController extends AbstractController
 {
     use InteractsWithResponseTrait;
@@ -26,15 +24,11 @@ class CampaignsController extends AbstractController
         $this->dotdigitalClientFactory = $dotdigitalClientFactory;
     }
 
-    /**
-     * @Route(
-     *     "/api/dotdigital/campaigns",
-     *     name="api.action.dotdigital.campaigns",
-     *     methods={"GET"}
-     * )
-     *
-     * @throws GuzzleException
-     */
+    #[Route(
+        '/api/dotdigital/campaigns',
+        name: 'api.action.dotdigital.campaigns',
+        methods: ['GET']
+    )]
     public function showCampaigns(): JsonResponse
     {
         $campaigns = [];
--- src/Storefront/Controller/SmsConsentCaptureController.php
+++ src/Storefront/Controller/SmsConsentCaptureController.php
@@ -13,11 +13,9 @@ use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-/**
- * @Route(defaults={"_routeScope"={"storefront"}})
- */
+#[Route(defaults: ['_routeScope' => ['storefront']])]
 class SmsConsentCaptureController extends StorefrontController
 {
     public function __construct(
