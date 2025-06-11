0d9e93a0e336f6a5d754b719a0c800aaec0637d7
Alastair Mucklow <Alastair.Mucklow@dotdigital.com>
2025-06-11 11:41:01 +0000

    Merged PR 51367: Update implementation of custom logger
    ## What's being changed
    
    Our implementation of a DotdigitalLogger has been updated to use monolog configuration.
    
    ## Why it's being changed
    
    Deprecation happened [in this commit](https://github.com/shopware/shopware/commit/6d677af7442fc7c8216979b890505a0a5f939e74). `Shopware\Core\Framework\Log\LoggerFactory` is removed in 6.6.0.0.
    
    ## How to review / test this change
    
    - Clear cache
    - Run any flow builder action
    - See a dotdigital_dev-2024-2-06.log file appear
    - See log lines added to this file
    - Test this in 6.5.0.0
    
    Related work items: #242487

--- src/DotdigitalFlow.php
+++ src/DotdigitalFlow.php
@@ -4,11 +4,37 @@ declare(strict_types=1);
 namespace Dotdigital\Flow;
 
 use Composer\Autoload\ClassLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Shopware\Core\Framework\Parameter\AdditionalBundleParameters;
 use Shopware\Core\Framework\Plugin;
 
 class DotdigitalFlow extends Plugin
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $locator = new FileLocator('Resources/config');
+
+        $resolver = new LoaderResolver([
+            new YamlFileLoader($container, $locator),
+            new GlobFileLoader($container, $locator),
+            new DirectoryLoader($container, $locator),
+        ]);
+
+        $configLoader = new DelegatingLoader($resolver);
+
+        $confDir = \rtrim($this->getPath(), '/') . '/Resources/config';
+
+        $configLoader->load($confDir . '/{packages}/*.yaml', 'glob');
+    }
+
     public function getAdditionalBundles(AdditionalBundleParameters $parameters): array
     {
         self::classLoader();
