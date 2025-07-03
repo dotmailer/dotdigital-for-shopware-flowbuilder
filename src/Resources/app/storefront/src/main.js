74c4a195846ddcc6bad03cdf054bafb92c66eb6c
Shaun Hogan <Shaun.Hogan@dotdigital.com>
2025-07-03 11:29:06 +0000

    Merged PR 66661: fix compat
    fix compat
    
    Related work items: #305900

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,4 +1,4 @@
 window.PluginManager.register('FormPhoneConsentLoader', () => import('./plugin/form-phone-consent-loader-plugin'), '.register-form');
-window.PluginManager.register('FormPhoneConsentLoaderSMS', () => import('./plugin/form-phone-consent-loader-plugin'), '.sms-consent-capture');
+window.PluginManager.register('FormPhoneConsentLoader', () => import('./plugin/form-phone-consent-loader-plugin'), '.sms-consent-capture');
 window.PluginManager.register('FormPhoneConsentHandlerPlugin', () => import('./plugin/form-phone-consent-toggle-plugin'), '[data-form-phone-consent]');
 window.PluginManager.override('FormValidation', () => import('./plugin/form-phone-validation-plugin'), '[data-form-validation]');
